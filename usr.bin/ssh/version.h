a449fdf9bdf542b144c6b68aa5838c83c7198f05
Yeri Kim <laplacianmatrix@gmail.com>
2024-05-30 23:31:00 +0900

    initial commit

---
+++ usr.bin/ssh/version.h
@@ -0,0 +1,3 @@
+/* $OpenBSD: version.h,v 1.101 2024/03/11 04:59:47 djm Exp $ */
+
+#define SSH_VERSION	"OpenSSH_9.7"
---
+++ usr.sbin/rpki-client/version.h
@@ -0,0 +1,3 @@
+/* $OpenBSD: version.h,v 1.22 2024/03/01 16:27:34 tb Exp $ */
+
+#define RPKI_VERSION	"9.0"
